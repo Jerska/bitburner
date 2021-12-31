3548feda47cea3054cc6fcf87b34b8730350f157
Matthieu Dumont <jerska@live.fr>
2021-12-31 17:43:02 +0100

    fix: remove extra disableLog

--- utils.daemon.js
+++ utils.daemon.js
@@ -1,7 +1,6 @@
 import { createLogger, createErrorLogger } from './utils.log.js';
 
 export function createDaemonRunner(ns, isDaemon, { cleanup = () => {}, sleepDuration = 1000 }) {
-  disableLog(ns, 'asleep');
   return async function daemonRun(fn) {
     const log = createLogger(ns, { isDaemon });
     const errorLog = createErrorLogger(ns, { isDaemon });
