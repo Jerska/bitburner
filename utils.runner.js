ae811134d131bcdd7919aa2b5b249283a395b72d
Matthieu Dumont <jerska@live.fr>
2022-01-02 04:45:56 +0100

    feat(runner): expose helpers as object

--- utils.runner.js
+++ utils.runner.js
@@ -4,14 +4,18 @@ export function createRunner(ns, isDaemon, { cleanup = () => {}, sleepDuration =
   return async function run(fn) {
     const log = createLogger(ns, { isDaemon });
     const errorLog = createErrorLogger(ns, { isDaemon });
+    let shouldStop = !isDaemon;
+    const stop = () => {
+      shouldStop = true;
+    };
     if (isDaemon) ns.tprint('Running daemon');
     do {
-      await fn(log, errorLog);
+      await fn({ log, errorLog, stop });
       await cleanup();
       if (isDaemon) {
         log(`Sleeping ${sleepDuration} ms`);
         await ns.asleep(sleepDuration);
       }
-    } while (isDaemon);
+    } while (shouldStop);
   };
 }
