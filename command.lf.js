9d03ccec9c4884d9424e0b1895bd38d3bf4a3c3b
Matthieu Dumont <jerska@live.fr>
2022-01-02 08:29:43 +0100

    feat(lf): add rm option

--- command.lf.js
+++ command.lf.js
@@ -2,9 +2,9 @@
  * Description: lf handles hacknet buying machines.
  *
  * Options:
- *   -d     Daemon mode: automatically buys next Hacknet upgrades every second.
+ *   --rm   Removes files found.
  */
-const USAGE = 'lf [<pattern>]';
+const USAGE = 'lf [--rm] [<patterns>]';
 
 import { parseArgs } from './utils.args.js';
 import { createRunner } from './utils.runner.js';
@@ -13,8 +13,9 @@ import { getHosts } from './utils.servers.js';
 export async function main(ns) {
   ns.disableLog('ALL');
 
-  const { args } = parseArgs(ns, { maxArgs: 1, USAGE });
-  const patterns = args[0] ? args[0].split(',') : [];
+  const { args, opts } = parseArgs(ns, { maxArgs: 1, USAGE });
+  const patterns = args;
+  const rm = opts.rm;
 
   const runner = createRunner(ns, false);
   await runner(async ({ log }) => {
@@ -26,7 +27,15 @@ export async function main(ns) {
       if (files.length > 0) {
         log(`Host: ${host}`);
         for (const f of files) {
-          log(`* File: ${f}`);
+          let suffix = '';
+          if (rm) {
+            suffix = ' [removed]';
+            const success = ns.rm(f, host);
+            if (!success) {
+              logError(`Couldn't remove ${f} from ${host}`);
+            }
+          }
+          log(`* File: ${f}${suffix}`);
         }
       }
       await ns.asleep(1);
