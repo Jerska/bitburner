a9fa80cd3b67aa796a4fc6a1f68962700a8c8eaf
Matthieu Dumont <jerska@live.fr>
2022-01-04 03:47:05 +0100

    feat(hack): add toast on successful hack

--- script.hack.js
+++ script.hack.js
@@ -1,3 +1,7 @@
 export async function main(ns) {
-  await ns.hack(ns.args[0]);
+  const host = ns.args[0];
+  const money = await ns.hack(host);
+  if (money > 0) {
+    ns.toast(`Hack: ${host} +${ns.nFormat(money, '0.0a')}$`, 'success');
+  }
 }
